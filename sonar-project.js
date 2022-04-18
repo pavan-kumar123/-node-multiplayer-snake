1991e8c6e04ad227d47caac7744769fbb64018cd
Pavan kumar <pavansdynasty@gmail.com>
2022-04-18 12:21:22 +0530

    updated ip

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://172.24.0.1:9000/',
+       serverUrl: 'http://http://172.18.3.141:9000/',
        options : {
        'sonar.sources': '.',
        //'sonar.inclusions' : '.' // Entry point of your code
